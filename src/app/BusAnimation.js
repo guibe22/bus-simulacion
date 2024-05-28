bfb7b4691d86ca2b7792f04f72b45db9bda2790d
Wilber Galvez <galvezwilber22@gmail.com>
2024-05-27 22:14:16 -0400

    first commit

--- src/app/BusAnimation.js
+++ src/app/BusAnimation.js
@@ -1,94 +1,101 @@
 'use client'
 import React, { useState } from 'react';
 import './BusAnimation.css'; // Importa el archivo de estilos CSS
+import { format } from 'date-fns';
 
 const BusAnimation = () => {
+    const [personasEnElBus, setPersonasEnElBus] = useState(0);
+    const [personasEnLaEstacionA, setPersonasEnLaEstacionA] = useState(0);
+    const [personasEnLaEstacionB, setPersonasEnLaEstacionB] = useState(0);
+    const [personasQueLlegaronALaEstacionA, setPersonasQueLlegaronALaEstacionA] = useState(0);
+    const [personasQueLlegaronALaEstacionB, setPersonasQueLlegaronALaEstacionB] = useState(0);
+    const [personasQueSeSubieronAlBus, setPersonasQueSeSubieronAlBus] = useState(0);
+    const [personasQueSeBajaronDelBus, setPersonasQueSeBajaronDelBus] = useState(0);
+    const initialTime = new Date('2024-05-27T22:05:02'); 
+
+    const [horaDeSalida, setHoraDeSalida] = useState(initialTime);
+    const [horaDeLlegada, setHoraDeLlegada] = useState(initialTime);
     const [isMovingForward, setIsMovingForward] = useState(true);
-    const [isAtStationB, setIsAtStationB] = useState(false);
 
-    const [passengersArrivingAtA, setPassengersArrivingAtA] = useState(0);
-    const [passengersArrivingAtB, setPassengersArrivingAtB] = useState(0);
-    const [passengersBoarding, setPassengersBoarding] = useState(0);
-    const [passengersAlighting, setPassengersAlighting] = useState(0);
-    const [peopleOnBus, setPeopleOnBus] = useState(0);
-    const [departureTime, setDepartureTime] = useState('');
-    const [arrivalTime, setArrivalTime] = useState('');
+    const [totalPersonasTransportadas, setTotalPersonasTransportadas] = useState(0);
+    const [totalPersonasQueLlegaronALaEstacionA, setTotalPersonasQueLlegaronALaEstacionA] = useState(0);
+    const [totalPersonasQueLlegaronALaEstacionB, setTotalPersonasQueLlegaronALaEstacionB] = useState(0);
+    const [totalPersonasQueSeSubieronAlBus, setTotalPersonasQueSeSubieronAlBus] = useState(0);
+    const [totalPersonasQueSeBajaronDelBus, setTotalPersonasQueSeBajaronDelBus] = useState(0);
+    const [totalPersonasQueSeQuedaronSinMontarse, setTotalPersonasQueSeQuedaronSinMontarse] = useState(0);
 
-    // Nuevos estados para el número de personas en cada estación
-    const [peopleAtStationA, setPeopleAtStationA] = useState(0);
-    const [peopleAtStationB, setPeopleAtStationB] = useState(0);
+    const options = { hour: '2-digit', minute: '2-digit', hour12: false };
 
-    const handleButtonClick = () => {
-        // Simular personas que se bajan del bus
-        const alighting = Math.floor(Math.random() * (peopleOnBus + 1)); // No pueden bajarse más personas de las que están en el bus
-        setPassengersAlighting(alighting);
 
-        // Actualizar el número de personas en el bus
-        const newPeopleOnBus = peopleOnBus - alighting;
-        setPeopleOnBus(newPeopleOnBus);
-
-        // Si estamos en la estación A, restamos las personas que se bajan de la estación A
-        if (!isAtStationB) {
-            const newPeopleAtStationA = peopleAtStationA - alighting;
-            setPeopleAtStationA(newPeopleAtStationA);
-        } else {
-            // Si estamos en la estación B, restamos las personas que se bajan de la estación B
-            const newPeopleAtStationB = peopleAtStationB - alighting;
-            setPeopleAtStationB(newPeopleAtStationB);
-        }
-
-        // Simular llegada de personas a la estación A
-        const arrivingAtA = Math.floor(Math.random() * 5);
-        setPassengersArrivingAtA(arrivingAtA);
-        setPeopleAtStationA(peopleAtStationA + arrivingAtA);
-
-        // Simular llegada de personas a la estación B
-        const arrivingAtB = Math.floor(Math.random() * 5);
-        setPassengersArrivingAtB(arrivingAtB);
-        setPeopleAtStationB(peopleAtStationB + arrivingAtB);
-
-        // Simular personas que se suben al bus
-        const boarding = Math.floor(Math.random() * 5);
-        setPassengersBoarding(boarding);
-
-        // Actualizar el número de personas en el bus
-        const newPeopleOnBusAfterBoarding = newPeopleOnBus + boarding;
-        setPeopleOnBus(newPeopleOnBusAfterBoarding);
-
-        // Actualizar la hora de salida y llegada
-        const departure = new Date().toLocaleTimeString();
-        setDepartureTime(departure);
-        const arrival = new Date().toLocaleTimeString();
-        setArrivalTime(arrival);
-
-        // Cambiar la dirección del movimiento
-        setIsMovingForward(!isMovingForward);
-        setIsAtStationB(!isAtStationB);
-    };
     const handleFinishButtonClick = () => {
-        // Mostrar los datos finales
-        alert(`Cantidad de personas que llegaron a la estación A: ${passengersArrivingAtA}\n
-               Cantidad de personas que llegaron a la estación B: ${passengersArrivingAtB}\n
-               Cantidad de personas que se subieron al bus: ${passengersBoarding}\n
-               Cantidad de personas que se bajaron del bus: ${passengersAlighting}\n
-               Cantidad de personas en el bus: ${peopleOnBus}\n
-               Hora de salida: ${departureTime}\n
-               Hora de llegada: ${arrivalTime}`);
+        alert(`Cantidad de personas que llegaron a la estación A: ${totalPersonasQueLlegaronALaEstacionA}\n
+               Cantidad de personas que llegaron a la estación B: ${totalPersonasQueLlegaronALaEstacionB}\n
+               Cantidad de personas que se subieron al bus: ${totalPersonasQueSeSubieronAlBus}\n
+               Cantidad de personas que se quedaron sin montarse: ${totalPersonasQueSeQuedaronSinMontarse}\n
+               Cantidad de personas en el bus: ${personasEnElBus}\n
+               `);
     };
-
+    const handleButtonClick = () => {
+       
+        const nuevasPersonasEnLaEstacionA = Math.floor(Math.random() * 10);
+        const nuevasPersonasEnLaEstacionB = Math.floor(Math.random() * 10);
+    
+       
+        setPersonasEnLaEstacionA(nuevasPersonasEnLaEstacionA);
+        setPersonasEnLaEstacionB(nuevasPersonasEnLaEstacionB);
+    
+        
+        const personasQueSeSubenAlBus = isMovingForward ? nuevasPersonasEnLaEstacionA : nuevasPersonasEnLaEstacionB;
+        const personasQueSeBajaronDelBus = Math.floor(Math.random() * personasEnElBus);
+    
+        const nuevasPersonasEnElBus = personasEnElBus + personasQueSeSubenAlBus - personasQueSeBajaronDelBus;
+        setPersonasEnElBus(nuevasPersonasEnElBus);
+    
+        
+        if (isMovingForward) {
+            setPersonasQueLlegaronALaEstacionA(nuevasPersonasEnLaEstacionA);
+            setPersonasQueSeSubieronAlBus(personasQueSeSubenAlBus);
+            setPersonasQueSeBajaronDelBus(personasQueSeBajaronDelBus);
+            setTotalPersonasQueLlegaronALaEstacionA(totalPersonasQueLlegaronALaEstacionA + personasQueSeSubieronAlBus);
+        } else {
+            setPersonasQueLlegaronALaEstacionB(nuevasPersonasEnLaEstacionB);
+            setPersonasQueSeSubieronAlBus(personasQueSeSubenAlBus);
+            setPersonasQueSeBajaronDelBus(personasQueSeBajaronDelBus);
+            setTotalPersonasQueLlegaronALaEstacionB(totalPersonasQueLlegaronALaEstacionB + personasQueSeSubieronAlBus);
+        }
+    
+        
+        setTotalPersonasTransportadas(totalPersonasTransportadas + personasQueSeSubenAlBus);
+        setTotalPersonasQueSeSubieronAlBus(totalPersonasQueSeSubieronAlBus + personasQueSeSubenAlBus);
+        setTotalPersonasQueSeBajaronDelBus(totalPersonasQueSeBajaronDelBus + personasQueSeBajaronDelBus);
+        setTotalPersonasQueSeQuedaronSinMontarse(personasEnLaEstacionA + personasEnLaEstacionB);
+    
+      
+        const nuevaHoraDeSalida = new Date(horaDeSalida);
+        nuevaHoraDeSalida.setMinutes(nuevaHoraDeSalida.getMinutes() + 30);
+        setHoraDeSalida(nuevaHoraDeSalida);
+    
+        const nuevaHoraDeLlegada = new Date(nuevaHoraDeSalida);
+        nuevaHoraDeLlegada.setMinutes(nuevaHoraDeLlegada.getMinutes() + 30);
+        setHoraDeLlegada(nuevaHoraDeLlegada);
+    
+        
+        setIsMovingForward(!isMovingForward);
+    };
+    
     return (
         <div className="relative w-full max-w-screen overflow-hidden h-64 bg-gray-200">
             {/* Información de personas en el bus y horas */}
             <div className="absolute top-4 left-1/2 transform -translate-x-1/2 z-10 bg-gray-300 px-4 py-2 rounded">
-                <p>Personas en el bus: {peopleOnBus}</p>
-                <p>Personas en la estación A: {peopleAtStationA}</p>
-                <p>Personas en la estación B: {peopleAtStationB}</p>
-                <p>Personas que llegaron a la estación A: {passengersArrivingAtA}</p>
-                <p>Personas que llegaron a la estación B: {passengersArrivingAtB}</p>
-                <p>Personas que se subieron al bus: {passengersBoarding}</p>
-                <p>Personas que se bajaron del bus: {passengersAlighting}</p>
-                <p>Hora de salida: {departureTime}</p>
-                <p>Hora de llegada: {arrivalTime}</p>
+                <p>Personas en el bus: {personasEnElBus}</p>
+                <p>Personas en la estación A: {personasEnLaEstacionA}</p>
+                <p>Personas en la estación B: {personasEnLaEstacionA}</p>
+                <p>Personas que llegaron a la estación A: {personasQueLlegaronALaEstacionA}</p>
+                <p>Personas que llegaron a la estación B: {personasQueLlegaronALaEstacionB}</p>
+                <p>Personas que se subieron al bus: {personasQueSeSubieronAlBus}</p>
+                <p>Personas que se bajaron del bus: {personasQueSeBajaronDelBus}</p>
+                <p>Hora de salida: {format(horaDeSalida, 'HH:mm:ss')}</p>
+                <p>Hora de llegada: {format(horaDeLlegada, 'HH:mm:ss')}</p>
             </div>
 
             {/* Botón para mover */}
@@ -113,7 +120,7 @@ const BusAnimation = () => {
             </div>
 
             {/* Estación de destino */}
-            <div className="absolute right-0 bottom-0 w-150 h-16 bg-red-500 flex items-center justify-center z-0">
+            <div className="absolute right-0 bottom-0 w-50 h-16 bg-red-500 flex items-center justify-center z-0">
                 Estación B
             </div>
 
@@ -121,11 +128,9 @@ const BusAnimation = () => {
             <img
                 src="https://static.vecteezy.com/system/resources/thumbnails/022/530/573/small/school-bus-isolated-cartoon-png.png"
                 alt="Bus"
-                className={`absolute bottom-0 w-30 h-12 transition-transform duration-10000 ${isMovingForward ? 'animate-bus-move-forward' : 'animate-bus-move-backward '
-                    }  ${isAtStationB ? 'transform scale-y-[-1]' : ''} z-0`}
+                className={`absolute bottom-0 w-30 h-12 transition-transform duration-1000 ${isMovingForward ? 'animate-bus-move-forward' : 'animate-bus-move-backward'}`}
             />
         </div>
-
     );
 };
 
